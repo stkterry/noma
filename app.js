1169faa363af51dbcbc5b0b7e69e5cd5fb8f1804
stkterry <stkterry@gmail.com>
2020-09-12 15:57:17 -0400

    Initial routes setup, debug config, structure

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+// App =========================================================================
 const app = express();
-
-const db = require('./server/config/keys').mongoURI;
-
-app.get("/", (req, res) => res.send("Hello World!!!"));
-
 const port = process.env.PORT || 5001;
+// -----------------------------------------------------------------------------
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// Database ====================================================================
+const db = require('./server/config/keys').mongoURI;
+const users = require('./server/routes/api/users');
 
 mongoose
-  .connect(db, {useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+// Config / emits
+app.get("/", (req, res) => res.send("Hello World!!!"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// API Routes
+app.use("/api/users", users);
+
+// Listeners
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// -----------------------------------------------------------------------------
