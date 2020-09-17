6182b95613fa5bf549c79521badf54b38405d19b
stkterry <stkterry@gmail.com>
2020-09-17 08:31:41 -0400

    Move process.ENV lines

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const passport = require("passport");
 const path = require('path');
 
 
+
+// App =========================================================================
+const app = express();
+const port = process.env.PORT || 5001;
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
-
-
-// App =========================================================================
-const app = express();
-const port = process.env.PORT || 5001;
 // -----------------------------------------------------------------------------
 
 // Database ====================================================================
